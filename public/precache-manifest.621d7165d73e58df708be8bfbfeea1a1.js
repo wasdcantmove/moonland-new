cca72a91349daa9d04ca39e757d354b2f0155610
Liam Waddell <liam.waddell@accedo.tv>
2022-04-28 22:00:58 +0800

    123

---
+++ public/precache-manifest.621d7165d73e58df708be8bfbfeea1a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e899e2857ccf41b1304539ad40c434d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0fc65803a548ba3a511",
+    "url": "/static/css/main.8313b0cc.chunk.css"
+  },
+  {
+    "revision": "3546df6f26c76fc7af2d",
+    "url": "/static/js/2.abdba1c6.chunk.js"
+  },
+  {
+    "revision": "13f8ad099a640a8dfcff3486af0da291",
+    "url": "/static/js/2.abdba1c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7973ae578fa2e18025b2",
+    "url": "/static/js/3.dfce25c8.chunk.js"
+  },
+  {
+    "revision": "a0fc65803a548ba3a511",
+    "url": "/static/js/main.0c13737e.chunk.js"
+  },
+  {
+    "revision": "033d4fce18ec8f1beead",
+    "url": "/static/js/runtime-main.217d9cf1.js"
+  },
+  {
+    "revision": "f45e90e0f9395282abaf753563b414e7",
+    "url": "/static/media/background.f45e90e0.png"
+  }
+]);
