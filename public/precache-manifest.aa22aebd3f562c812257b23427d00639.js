dba8cd72f14ce835f7e14abf83489da8cd2bfb8e
Liam Waddell <liam.waddell@accedo.tv>
2022-05-06 20:28:14 +0800

    hadi

---
+++ public/precache-manifest.aa22aebd3f562c812257b23427d00639.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfed393fd2d07d757b3d4518d36d42bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "077094cc232c3616ef33",
+    "url": "/static/css/main.8313b0cc.chunk.css"
+  },
+  {
+    "revision": "3546df6f26c76fc7af2d",
+    "url": "/static/js/2.abdba1c6.chunk.js"
+  },
+  {
+    "revision": "13f8ad099a640a8dfcff3486af0da291",
+    "url": "/static/js/2.abdba1c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7973ae578fa2e18025b2",
+    "url": "/static/js/3.dfce25c8.chunk.js"
+  },
+  {
+    "revision": "077094cc232c3616ef33",
+    "url": "/static/js/main.c0e85483.chunk.js"
+  },
+  {
+    "revision": "033d4fce18ec8f1beead",
+    "url": "/static/js/runtime-main.217d9cf1.js"
+  },
+  {
+    "revision": "f45e90e0f9395282abaf753563b414e7",
+    "url": "/static/media/background.f45e90e0.png"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6f3814f6b02ad43f46926f39c9c7afee.js"
+  "/precache-manifest.aa22aebd3f562c812257b23427d00639.js"
 );
 
 self.addEventListener('message', (event) => {
